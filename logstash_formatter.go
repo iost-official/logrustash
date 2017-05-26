866a056378c27c2f69911d94df4327e8140a498e
Alexander <cheshir@programist.ru>
2017-05-26 14:16:49 +0300

    Added auto reconnects.

--- logstash_formatter.go
+++ logstash_formatter.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/sirupsen/logrus"
+	"github.com/Sirupsen/logrus"
 )
 
 // LogstashFormatter generates json in logstash format.
--- logstash_formatter_test.go
+++ logstash_formatter_test.go
@@ -7,7 +7,7 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/sirupsen/logrus"
+	"github.com/Sirupsen/logrus"
 )
 
 func TestLogstashFormatter(t *testing.T) {
